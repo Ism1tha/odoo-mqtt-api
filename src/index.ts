af356665aebdffc0c628f9daa6ffa8cf14ba446c
Ism1tha <xgalan@hotmail.com>
2025-05-23 00:18:22 +0200

    feat: update development scripts and enhance TypeScript configuration for improved module handling

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-console.log('Hello, world!');
-console.log('This is a TypeScript project.');
+import 'dotenv/config';
+
+console.log('Hello, world! This is a test.');
+console.log('Bind address:', process.env.BIND_ADDRESS);
+console.log('Bind port:', process.env.BIND_PORT);
