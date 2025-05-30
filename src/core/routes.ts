dd40bc77692d37dd06b7e9fe6975a674edb65dfb
Ism1tha <xgalan@hotmail.com>
2025-05-30 23:51:27 +0200

    refactor(routes): remove unused hello route for cleaner API

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -4,8 +4,4 @@ import { robotRouter } from '../modules/robot/index.js';
 
 export const apiRouter: express.Router = express.Router();
 
-apiRouter.get('/hello', (req: express.Request, res: express.Response) => {
-  res.json({ message: 'Hello World!' });
-});
-
 apiRouter.use('/robots', robotRouter);
