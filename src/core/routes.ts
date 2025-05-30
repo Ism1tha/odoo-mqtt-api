a34f677c3de683b76cce523a54a3bd42ede5bdc2
Ism1tha <xgalan@hotmail.com>
2025-05-30 23:50:54 +0200

    refactor(routes): add type annotations for express request and response

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 import { robotRouter } from '../modules/robot/index.js';
 
-export const apiRouter = express.Router();
+export const apiRouter: express.Router = express.Router();
 
-apiRouter.get('/hello', (req, res) => {
+apiRouter.get('/hello', (req: express.Request, res: express.Response) => {
   res.json({ message: 'Hello World!' });
 });
 
